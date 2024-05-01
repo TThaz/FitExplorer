339e50a4862cc021241cca27097566664dd80106
Thalles Azevedo <TThaz>
2024-05-01 10:00:03 -0300

    Criação da página de criação de usuários

---
+++ src/pages/PaginaCadastroUsuarios/PaginaCadastroUsuarios.jsx
@@ -0,0 +1,9 @@
+import styles from "./styles.module.css"
+
+function PaginaCadastroUsuarios() {
+    return (
+        <h1 className={styles.header}>PaginaCadastroUsuarios</h1>
+    )
+}
+
+export default PaginaCadastroUsuarios;
