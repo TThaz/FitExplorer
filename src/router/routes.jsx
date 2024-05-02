8fbea859c513ae1aeb708bc28f5a9dea3c5e774e
Thalles Azevedo <TThaz>
2024-05-01 22:56:22 -0300

    Criação de Rotas

---
+++ src/router/routes.jsx
@@ -0,0 +1,39 @@
+import { createBrowserRouter } from "react-router-dom"
+
+import App from "../App.jsx"
+import PaginaCadastroUsuarios from "../pages/PaginaCadastroUsuarios/PaginaCadastroUsuarios"
+import PaginaDashboard from "../pages/PaginaDashboard/PaginaDashboard"
+import PaginaCadastroLocais from "../pages/PaginaCadastroLocais/PaginaCadastroLocais"
+import PaginaLogin from "../pages/PaginaLogin/PaginaLogin"
+import ListaLocais from "../pages/ListaLocais/ListaLocais"
+
+const routes = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                path: "/",
+                element: <PaginaDashboard />
+            },
+            {
+                path: "/cadastro-locais",
+                element: <PaginaCadastroLocais />
+            },
+            {
+                path: "/user-login",
+                element: <PaginaLogin />
+            },
+            {
+                path: "/user-register",
+                element: <PaginaCadastroUsuarios />
+            },
+            {
+                path: "/lista-locais",
+                element: <ListaLocais />
+            }
+        ]
+    }
+])
+
+export default routes;
