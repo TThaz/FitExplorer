8b5fd288356f0886b1a9ebed984a9cc2c8a90ae4
Thalles Azevedo <TThaz>
2024-05-04 09:09:08 -0300

    criação dos dados de login

--- src/pages/PaginaLogin/PaginaLogin.jsx
+++ src/pages/PaginaLogin/PaginaLogin.jsx
@@ -1,8 +1,24 @@
 import styles from "./styles.module.css"
+import { Link } from "react-router-dom"
 
 function PaginaLogin() {
     return (
-        <h1 className={styles.header}>PaginaLogin</h1>
+        <div className="container"> 
+            <h1 className={styles.header}>Login</h1>
+
+            <div>
+                <label htmlFor="username"></label>
+                <input type="text" name="username" placeholder="Nome de usuário" />
+            </div>
+            <div>
+                <label htmlFor="password"></label>
+                <input type="password" name="password" placeholder="Senha" />
+            </div>
+            <div>
+                <button>Cadastrar</button>
+                <Link to="/dashboard"><button>Voltar</button></Link>
+            </div>
+        </div>
     )
 }
 
