381b12b07fb8c63d87aa85c461d89b5b5a74f6c2
Thalles Azevedo <TThaz>
2024-05-05 05:58:09 -0300

    Configuração useFetch

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -3,17 +3,16 @@ import { useState, useEffect } from "react";
 
 function useFetch(url) {
 
-    const [users, setUsers] = useState([]);
+    const [dados, setDados] = useState([]);
 
     useEffect(() => {
-        
         fetch(url)
             .then((response) => response.json())
-            .then((data) => setUsers(data)) 
+            .then((data) => setDados(data)) 
             .catch((error) => console.log(error))
     }, [url]) 
 
-    return users;
+    return dados;
 }
 
 export default useFetch;
