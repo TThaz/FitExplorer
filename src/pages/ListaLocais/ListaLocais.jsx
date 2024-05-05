458ac86a86401327a266fd5ee1e055337d6de32f
Thalles Azevedo <TThaz>
2024-05-05 06:02:48 -0300

    Configuração para listar os locais como cards

--- src/pages/ListaLocais/ListaLocais.jsx
+++ src/pages/ListaLocais/ListaLocais.jsx
@@ -1,8 +1,24 @@
-import styles from "./styles.module.css"
+import CardLocal from "../../components/CardLocal";
+import styles from "./styles.module.css";
+import { ExercisesContext } from "../../contexts/ExercisesContext";
+import { useContext } from "react";
 
 function ListaLocais() {
+
+    const { locals } = useContext(ExercisesContext)
+
     return (
-        <h1 className={styles.header}>ListaLocais</h1>
+        <div>
+                <div className={styles.rectangle}><div className={styles.rectangleOverlay}></div></div>
+                <p className={styles.mainTitle}>Explore trilhas incríveis</p>
+                <div>
+                    {Array.isArray(locals) &&
+                      locals.map((local, index) => (
+                        <CardLocal dadosLocal={local} key={index} />
+                      ))
+                    }
+                </div>
+        </div>
     )
 }
 
